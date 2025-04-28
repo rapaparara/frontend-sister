0e9a84f6cac27cb43cc00519f09413fcdd275bf0
rapaparara <rafiblue6@gmail.com>
2025-04-28 14:44:03 +0800

    feat(API KEY): Implementasi api key sekolah ke dalam env. penerapan di get user.

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,9 +3,11 @@ import api from './api'
 export const getUsers = async () => {
    try {
       const token = localStorage.getItem('access_token')
+      const apiKey = import.meta.env.VITE_API_KEY
       const response = await api.get('/user', {
          headers: {
             Authorization: `Bearer ${token}`,
+            'x-api-key': apiKey,
          },
       })
       return response.data
